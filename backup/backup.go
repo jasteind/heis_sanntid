a01edbe7836ccbd1b1c8efa7bd67028aef539100
jennysteindal <jenny.steindal@gmail.com>
2017-03-12 22:15:03 +0100

    Add files via upload

--- backup/backup.go
+++ backup/backup.go
@@ -1,1 +1,28 @@
+package backup
 
+import(
+	"os"
+	"encoding/json"
+	"fmt"
+	. "../types"
+)
+
+
+
+func SaveOrder(e ElevData_s){
+	file, _ := os.Create("backup_local.json")
+	buffer, _ := json.Marshal(e.Orders)
+	file.WriteAt(buffer,0)
+	fmt.Println("Saved orders: ")
+	fmt.Println(string(buffer))
+}
+
+
+func LoadOrder(e *ElevData_s){
+	file,_ := os.Open("backup_local.json")
+	buffer := make([]byte, 1024)
+	n, _ := file.ReadAt(buffer,0)
+	json.Unmarshal(buffer[:n], &e.Orders)
+	fmt.Println("Loaded orders: ")
+	fmt.Println(string(buffer))
+}
