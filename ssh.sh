7e0d9354a8af037249f5cd70f9e74ce4556a16f6
jennysteindal <jenny.steindal@gmail.com>
2017-03-14 21:23:25 +0100

    Add files via upload

--- ssh.sh
+++ ssh.sh
@@ -1,13 +1,24 @@
 #/bin/bash
 
-go build main.go
+# For første gang du skal bygge
+#For å åpne virtuelt vindu										endre her
+#gnome-terminal --title "virtual_2: 161" -x ssh student@129.241.187.161 &
+##mkdir "gr50"
+##cd "gr50"
+##touch mainrun
+##sudo chmod 777 mainrun
+#go build main.go
+#scp main student@129.241.187.161:gr50/mainrun
+##./mainrun  <- kjører koden på andre pcen
+##############
 
-scp main student@129.241.187.144:gr9/mainrun
-#scp main student@129.241.187.141:gr9/mainrun
 
-#scp -r /home/student/Desktop/Sanntid/Elevator_project_gr_16/main student@129.241.187.161:gruppe16/main
+#Etter endring av kode
+#go build main.go
+#scp main student@129.241.187.161:gr50/mainrun
+##./mainrun  <- kjører koden på andre pcen
+###############
+
+#scp -r /home/student/Desktop/Sanntid/Elevator_project_gr_16/main student@129.241.187.161:gruppe16
 
 
-#gnome-terminal --title "virtual_2: 154" -x ssh student@129.241.187.154 &
-#gnome-terminal --title "virtual_3: 144" -x ssh student@129.241.187.144 &
-#sudo chmod 777 mainrun
